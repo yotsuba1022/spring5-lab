50a1ad912a4f6edc25a7273a3f6c20e9eef92b60
Carl <yotsuba1022@gmail.com>
2018-02-26 23:58:51 +0800

    Init commit.

---
+++ src/main/java/idv/carl/spring5lab/Spring5LabApplication.java
@@ -0,0 +1,12 @@
+package idv.carl.spring5lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5LabApplication.class, args);
+	}
+}
